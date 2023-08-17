5c55efc592e69f301ae4211a331c9a41d336d936
Vashurin Ilya <119150424+UncleBillyCooper@users.noreply.github.com>
2023-08-17 09:55:22 +0700

    Update mainButton.vue

--- src/components/mainButton.vue
+++ src/components/mainButton.vue
@@ -49,7 +49,7 @@
 }
  button{
   /*width: 50vw;*/
-  width: clamp(150px, 70dvw, 684); 
+  width: 100%; 
   height: 8vw;
   border-radius: 10px;
   background: linear-gradient(135deg, rgba(49,38,177,0.65) 0%, rgba(255,0,10,0.87) 60%, rgba(36,43,220,0.61) 100%);
